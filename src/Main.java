dce6ebc76ae29327ee6364041b7ed0d3cc534136
Joshua Cruz <jcruz1998@gmail.com>
2024-10-24 18:47:38 -0400

    Dynamic Arrays

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+
+        DynamicArray dynamicArray = new DynamicArray(5);
+
+        dynamicArray.add("A");
+        dynamicArray.add("B");
+        dynamicArray.add("C");
+
+        System.out.println(dynamicArray);
+    }
+}
